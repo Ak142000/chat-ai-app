a4a0873af4aff2c4e9e1a487cf9a4d1447a681f3
Ajith Kumar <ajithk0213@gmail.com>
2025-09-20 10:56:48 +0530

    Remove merge conflict markers from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";   // ✅ make sure this is here
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
->>>>>>> 59a520c11134bedcc2cc2a6d7489c97b6f6fd814
+
