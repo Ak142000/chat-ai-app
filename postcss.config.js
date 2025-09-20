9a78db567fc16f79e7a690643eec7a2eb1279352
Ajith Kumar <ajithk0213@gmail.com>
2025-09-20 10:46:09 +0530

    Fix PostCSS config merge conflict

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-export default {
-=======
 module.exports = {
->>>>>>> 59a520c11134bedcc2cc2a6d7489c97b6f6fd814
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: {}, // keep this if you want CSS minification
   },
 }
